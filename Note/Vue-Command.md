4b69f9fb9737b9d31211e8425e49af0b72b02fad
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-15 11:21:26 +0700

    npm vye router

--- Note/Vue-Command.md
+++ Note/Vue-Command.md
@@ -13,7 +13,6 @@ git branch -m master main
 ### Tailwinds
 https://tailwindcss.com/docs/installation/using-vite
 
+### VUE-Router
+npm i vue-router@next
 
-### SRC
-https://www.youtube.com/watch?v=cfiN8lCA3RM&t=1431s&pp=ygUOdnVlIGpzIHByb2plY3Q%3D
-9.21
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="bg-blue-500 text-xl p-4 text-red-200">
+  <div class="bg-blue-500 text-xl p-4 text-red-600">
     Tailwind CSS is working! 🎉
   </div>
+  <div class="p-8 pb-0 text-orange-500">
+    <h1 class="text-4xl font-bold mb-4">Random Meals</h1>
+  </div>
+  <Meals :meals="meals" />
 </template>
 
 <script setup>
